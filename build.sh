bd24f629c20b52628511460c30ee11cdf687890a
Austin Eng <enga@chromium.org>
2023-01-31 09:10:50 -0800

    Update build.yml

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x # echo on
 mkdir -p bin
-clang++ stencil_tests.mm -std=c++17 -framework Metal -fobjc-arc -o bin/stencil_tests
+clang++ stencil_tests.mm -mmacosx-version-min=10.12 -std=c++17 -framework Metal -fobjc-arc -o bin/stencil_tests
